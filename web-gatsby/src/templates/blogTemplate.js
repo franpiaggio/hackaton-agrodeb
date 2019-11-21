b5bf9d4ec3fa3fe407cffc8411f8d5bdc7ab1379
Fran <fpiaggio92@gmail.com>
2019-11-21 11:41:28 -0300

    El mejor blog de tu vida

--- web-gatsby/src/templates/blogTemplate.js
+++ web-gatsby/src/templates/blogTemplate.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -10,13 +11,15 @@ export default function Template({
     <Layout>
       <div className="blog-post-container">
         <div className="blog-post">
-          <h1>{frontmatter.title}</h1>
+          <h1 className="title">{frontmatter.title}</h1>
           <h2>{frontmatter.date}</h2>
           <div
             className="blog-post-content"
             dangerouslySetInnerHTML={{ __html: html }}
           />
         </div>
+        <hr />
+        <Link to="/team">Volver</Link>
       </div>
     </Layout>
   )
